42c388c9a9cae3740bf983985fa17cbfc9ed5767
dimitristeave <153617899+dimitristeave@users.noreply.github.com>
2024-11-25 20:43:23 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:isibappmoodle/views/home_share_file_view'; // Assurez-vous que ce fichier contient la page HomeShareFile
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, // Supprime le bandeau de débogage
+      title: 'ShareFile App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue, // Couleur principale de l'application
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeShareFile(), // Écran d'accueil de l'application
+    );
+  }
+}
